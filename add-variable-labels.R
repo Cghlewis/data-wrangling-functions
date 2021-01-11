075cd3bd812663a93df0c30aa160b7c1e78d84c3
Cghlewis <cgh.lewis@gmail.com>
2021-01-11 14:52:35 -0600

    Adding to variable labels

--- add-variable-labels.R
+++ add-variable-labels.R
@@ -2,6 +2,7 @@
 # library(labelled)
 # library(sjlabelled)
 # library(tidyverse)
+# library(snakecase)
 
 d <- tibble::tribble(
   ~Var1, ~Var2, ~Var3,
@@ -22,6 +23,13 @@ d3 <- tibble::tribble(
   "d", 1, 4,
   "e", 2, 2,
   "f", 4, 1,
+)
+
+d4 <- tibble::tribble(
+  ~student_id, ~map_ela_20, ~map_math_20,
+  1, 100, 400,
+  2, 200, 200,
+  3, 400, 100,
 )
 
 
@@ -62,9 +70,46 @@ d3 <- d3 %>% copy_labels_from(d)
 d3 %>% var_label()
 
 
+# labelled::set_variable_labels -------------------------------------------
+
+  ## Deriving variable names from variable labels
+
+  ## Simple example:
+
+d4 <- d4 %>% set_variable_labels(.labels=names(d4))
+
+d4 %>% var_label()
+
+  ## Add sentence case
+
+d4 <- d4 %>% set_variable_labels(.labels=snakecase::to_sentence_case(names(d4)))
+
+d4 %>% var_label()
+
+  ## Adding additional info to labels
+
+d4 <- d4 %>% 
+  set_variable_labels(
+    .labels = str_replace_all(names(.),c("_"=" ", 
+                                         "map"="map score",
+                                         "math"="in mathematics",
+                                         "ela"= "in english language arts",
+                                         "20"="19-20 school year")))
+
+  ## OR if you want to use sentence case again
+
+d4 <- d4 %>% 
+  set_variable_labels(
+    .labels = snakecase::to_sentence_case(str_replace_all(names(.),c("_"=" ", 
+                                         "map"="map score",
+                                         "math"="in mathematics",
+                                         "ela"= "in english language arts",
+                                         "20"="19-20 school year"))))
+
+
 # sjlabelled::copy_labels ----------------------------------------------
 
-  ## Note: I prefer sticking with all labelled b/c of the following reasons:
+  ## Note: I prefer sticking with all labelled functions b/c of the following reasons:
   ## 1. Labelled exports best with `haven` which is what I prefer to use
   ## 2. With sjlabelled you need to export with sjlabelled::write_spss for best outcome
   ## 3. Things don't export well when labelled and sjlabelled are combined in a data cleaning script

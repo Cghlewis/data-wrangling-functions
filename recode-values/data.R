efe814ede2c2e92e25a6a62f8a8ba2efc3db1ee1
Cghlewis <cgh.lewis@gmail.com>
2021-04-02 14:42:40 -0500

    add recode values

---
+++ recode-values/data.R
@@ -0,0 +1,40 @@
+
+
+library(tidyverse)
+
+d1 <- tibble::tribble(
+  ~Var1, ~Var2, ~Var3,
+  "a", 2, 3.6,
+  "b", NA, 8.5,
+  "c", 3, NA,
+)
+
+d2 <- tibble::tribble(
+  ~Var1, ~Var2, ~Var3,
+  "a", 1, 4,
+  "b", NA, 5,
+  "c", 3, 1,
+)
+
+d3 <- tibble::tribble(
+  ~Var1, ~Var2, ~Var3,
+  "a", 1, 2,
+  "b", -999, 0,
+  "c", 3, -999,
+)
+
+d4 <- tibble::tribble(
+  ~id, ~gender, ~lunch,
+  "a", "m", "f",
+  "b", "f", "r",
+  "c", "n", "p",
+)
+
+d5 <- tibble::tribble(
+  ~Var1, ~Var2, ~Var3,
+  "a", 1, 2,
+  "b", 16, 0,
+  "c", 3, 1,
+)
+
+

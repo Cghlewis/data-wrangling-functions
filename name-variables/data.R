d4afbe4905e8b388fa717ab113a309b6cb8a2dca
Cghlewis <cgh.lewis@gmail.com>
2021-04-21 13:04:06 -0500

    add name variables

---
+++ name-variables/data.R
@@ -0,0 +1,34 @@
+
+
+library(tidyverse)
+
+d1 <- tibble::tribble(
+  ~Var1, ~Var2, ~Var3,
+  "a", 2, 3.6,
+  "b", NA, 8.5,
+  "c", 3, NA,
+)
+
+d2 <- tibble::tribble(
+  ~Var.1, ~Var.2, ~Var.3,
+  "a", 1, 4,
+  "b", NA, 5,
+  "c", 3, 1,
+)
+
+d3 <- tibble::tribble(
+  ~`Var 1`, ~`Var 2`, ~`Var 3`,
+  "a", 1, 2,
+  "b", -999, 0,
+  "c", 3, -999,
+)
+
+d4 <- tibble::tribble(
+  ~stu_id, ~stu_gender, ~`lunch status`,
+  "a", "m", "f",
+  "b", "f", "r",
+  "c", "n", "p",
+)
+
+
+

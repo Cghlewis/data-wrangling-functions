99b04042328607ad29c04a5dd375a02e9954cd13
Cghlewis <cgh.lewis@gmail.com>
2022-08-20 16:31:53 -0500

    Add examples to label NA, calculate row values, and create variables

---
+++ create-variables/data.R
@@ -0,0 +1,7 @@
+
+library(tidyverse)
+
+d1 <- tibble::tribble(~id, ~item1, ~item2, ~item3, ~item4,
+                      10, 3, 5, 3, 5,
+                      11, 3, 5, 1, 5,
+                      12, 3, 1, 3, 5)

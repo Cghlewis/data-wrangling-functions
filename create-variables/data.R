b16a68cfa925f26d968dd53af2eec581137eadf1
Cghlewis <cgh.lewis@gmail.com>
2022-08-20 16:52:23 -0500

    clarify na values for create variables

--- create-variables/data.R
+++ create-variables/data.R
@@ -2,6 +2,6 @@
 library(tidyverse)
 
 d1 <- tibble::tribble(~id, ~item1, ~item2, ~item3, ~item4,
-                      10, 3, 5, 3, 5,
+                      10, 3, 5, 3, NA,
                       11, 3, 5, 1, 5,
                       12, 3, 1, 3, 5)

16740facf3f4899405ed3e53842e4733d75e4969
Cghlewis <cgh.lewis@gmail.com>
2022-08-22 09:35:20 -0500

    Add na label md and add create variable example

--- create-variables/data.R
+++ create-variables/data.R
@@ -5,3 +5,10 @@ d1 <- tibble::tribble(~id, ~item1, ~item2, ~item3, ~item4,
                       10, 3, 5, 3, NA,
                       11, 3, 5, 1, 5,
                       12, 3, 1, 3, 5)
+
+d2 <- tibble::tribble(~id, ~cat,
+                      10, "1, 4, 11",
+                      11, "4, 5, 10",
+                      12, "5, 2, 11",
+                      13, "5, 1",
+                      14, NA_character_)

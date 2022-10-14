2de4bc8173878bfb5822fb2e5fb832569539bcc8
Cghlewis <cgh.lewis@gmail.com>
2022-10-14 11:28:29 -0500

    Add reorder variables using a dictionary

--- reorder/data.R
+++ reorder/data.R
@@ -10,7 +10,7 @@ d1 <- tibble::tribble(
 )
 
 d2 <- tibble::tribble(
-  ~s_id, ~`s_gender`, ~`s_grade`, 
+  ~s_id, ~s_gender, ~s_grade, 
   "a", "m", "K",
   "b", "f", "1",
   "c", "n", "3",
@@ -18,7 +18,7 @@ d2 <- tibble::tribble(
 )
 
 d3 <- tibble::tribble(
-  ~s_id, ~`s_gender`, ~`s_grade`, 
+  ~s_id, ~s_gender, ~s_grade, 
   "a", "m", 0,
   "b", "f", 1,
   "c", "n", 3,
@@ -27,11 +27,43 @@ d3 <- tibble::tribble(
 
 
 d4 <- tibble::tribble(
-  ~s_id, ~`t_last_name`, ~`t_first_name`, 
+  ~s_id, ~t_last_name, ~t_first_name, 
   10, "simpson", "homer",
   15, "simpson", "marge",
   12, "simpson", "homer",
   13, "flanders", "marge"
+)
+
+d5 <- tibble::tribble(
+  ~s_id, ~t_last_name, ~t_first_name, ~grade,
+  10, "simpson", "homer", 4,
+  15, "simpson", "marge", 5,
+  12, "simpson", "homer", 3,
+  13, "flanders", "marge", 2
+)
+
+dict <- tibble::tribble(
+  ~var_name, ~label,
+  "s_id", "student id",
+  "grade", "grade level",
+  "t_first_name", "teacher first name",
+  "t_last_name", "teacher last name"
+)
+
+dict2 <- tibble::tribble(
+  ~var_name, ~label,
+  "grade", "grade level",
+  "t_first_name", "teacher first name",
+  "t_last_name", "teacher last name"
+)
+
+dict3 <- tibble::tribble(
+  ~var_name, ~label,
+  "s_id", "student id",
+  "grade", "grade level",
+  "t_first_name", "teacher first name",
+  "t_last_name", "teacher last name",
+  "school_id", "school id",
 )
 
 

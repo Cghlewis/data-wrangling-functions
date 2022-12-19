21f47e1e3c7cc503affcb166fac2e692702acdb1
Cghlewis <cgh.lewis@gmail.com>
2022-12-19 15:50:20 -0600

    Update dummy vars and separate

--- create-variables/data.R
+++ create-variables/data.R
@@ -21,6 +21,8 @@ d3 <- tibble::tribble(~id, ~cert,
                      13, "special ed",
                      14, "secondary ed")
 
+
+
 d4 <- tibble::tribble(~id, ~dob, ~test_date,
                       10, "2010-11-12", "2021-10-14",
                       11, "2011-07-08", "2021-10-15",
@@ -38,3 +40,10 @@ d6 <- tibble::tribble(~id, ~ grade, ~cut_scale1, ~cut_scale2, ~cut_scale3, ~scal
                       3, 3, 2, 3, 2, 3, 1, 2,
                       4, NA, NA, NA, NA, 3, 4, 1,
                       5, 2, 2.5, 3.1, 3, 4, 2, NA)
+
+d7 <- tibble::tribble(~id, ~cert, ~age,
+                      10, "elem ed, secondary ed", 30,
+                      11, "special ed", 42,
+                      12, "elem ed, special ed, secondary ed", 23,
+                      13, "special ed, secondary ed", 51,
+                      14, "secondary ed", 29)

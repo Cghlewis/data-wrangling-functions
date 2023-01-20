e61d6070af6f0f4ae408c71113ff3cd02632c7af
Cghlewis <cgh.lewis@gmail.com>
2023-01-20 06:22:40 -0600

    Add another example for duplicates

--- duplicates/data.R
+++ duplicates/data.R
@@ -13,3 +13,16 @@ d1 <- tibble::tribble(~stu_id, ~date, ~item1, ~item2, ~item3,
 d1 <- d1 %>%
   mutate(date = as.Date(date))
 
+
+d2 <- tibble::tribble(~stu_id, ~date, ~item1, ~item2, ~item3,
+                      100, "2022-11-15", 3, 4, 2,
+                      103, "2022-11-15", 2, 5, NA,
+                      105, "2022-11-15", 5, 4, 3,
+                      106, "2022-11-16", 2, 1, 1,
+                      100, "2022-11-17", 2, NA, NA,
+                      102, "2022-11-18", 3, 4, 1,
+                      103, "2022-11-18", 2, 4, 2,
+                      105, "2022-11-18", 5, 4, 1)
+
+d2 <- d2 %>%
+  mutate(date = as.Date(date))
